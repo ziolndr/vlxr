4d2647e269506120ca8a787455747fd561d5b912
ziolndr <ziolndr@gmail.com>
2014-02-10 00:30:31 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Vlxr::Application.routes.draw do
 	root 'landingpage#index'
-	post '/' => 'landingpage#index'
+	post '/' => 'subscribee#subscribe'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
