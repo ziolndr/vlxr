a4a20651b430e9e94eee41b0c9b57ebbfc5107bf
ziolndr <ziolndr@gmail.com>
2014-02-11 23:09:08 -0800

    fixed notif mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,11 @@
 class NotificationMailer < ActionMailer::Base
-	default :from -> "hivlxr@vlxr.co"
+	default from: "hivlxr@vlxr.co"
 
 	def subscription_notification(subscriber)
 
 		@subscriber = subscriber
 
-		mail(:to => subscriber.email, 
-			:subject => "Hi, thanks for signing up",
-			:unsub_link => unsub_email_link(@subscriber.email_token))
+		mail(:to => @subscriber.email, 
+				:subject => "Hi, thanks for signing up",
+				:unsub_link => unsub_email_link(@subscriber.email_token))
 	end
