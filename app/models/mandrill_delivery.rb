fdae6ae6fe1fad82e3090682806e53414aaff359
ziolndr <ziolndr@gmail.com>
2014-02-11 22:56:02 -0800

    updated mandrill.rb

--- app/models/mandrill_delivery.rb
+++ app/models/mandrill_delivery.rb
@@ -1,60 +1,60 @@
 require 'mandrill'
 
 class MandrillDelivery
-	attr_accessor :message
+  attr_accessor :message
 
-	def initialize(mail)
-	end
+  def initialize(mail)
+  end
 
-	def deliver!(mail)
-		build_meta_mandrill(mail)
-		send_mandrill(mail)
-	end
+  def deliver!(mail)
+    build_meta_mandrill(mail)
+    send_mandrill(mail)
+  end
 
-	def build_meta_mandrill(mail)
+#Message stuff
 
-		@message = {
-			:from_name=> "VLXR",
-			:from_email=>"hivlxr@vlxr.com",
-			:subject=> "#{mail['subject']}",
-			:to=>[
-				{
-					:email=>"#{mail['to']}",
-					:name=>"#{mail['name']}"
-					}
-					],
-			:auto_text => true,
-			:global_merge_vars => [
-									{
-										:name => "VLXR",
-										:content => "VLXR"
-									}
-									]
-		}
+  def build_meta_mandrill(mail)
 
-		true
-	end
+    #build Mandrill message hash
+    @message = {
+      :from_name=> "Admin",
+      :from_email=>"admin@somewhere.com",
+      :subject=> "#{mail['subject']}",
+      :to=>[
+            {
+              :email=> "#{mail['to']}",
+              :name=> "#{mail['name']}"
+            }
+           ],
+      :auto_text => true,
+      :global_merge_vars => [
+                             {
+                               :name => "LISTCOMPANY",
+                               :content => "Company Name Here"
+                             }
+                            ]
+    }
 
-	def send_mandrill(mail)
-		m = Mandrill::API.new
+    true
+  end
 
-		sending = m.messages.send_template 'Sign Up Confirmation',
-											[
-											 {
-											 	:name => 'main',
-											 	:content"#{mail.body}"
-											 },
-											 {
-											 	:name => 'unsub',
-											 	:content => "#{mail['unsub_link']}"
-											 }
-											],
-											message = @message)				
-		Rails.logger.info sending
-	end
+  #sends email via Mandrill
+  def send_mandrill(mail)
+    m = Mandrill::API.new
+
+    sending = m.messages.send_template('Sign-Up Confirmation',
+                                       [
+                                        {
+                                          :name => 'main',
+                                          :content => "#{mail.body}"
+                                        },
+                                        {
+                                          :name => 'unsub',
+                                          :content => "#{mail['unsub_link']}"
+                                        }
+                                       ],
+                                       message = @message)
+    Rails.logger.info sending
+  end
 
 end
-
-
-
-
