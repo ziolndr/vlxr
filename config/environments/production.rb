5e34bff4a57243263f6886a8363f325b049e76d2
ziolndr <ziolndr@gmail.com>
2014-02-18 00:30:09 -0800

    prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Vlxr::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  config.assets.precompile += %w( application.js )
+  # config.assets.precompile += %w( application.js )
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
